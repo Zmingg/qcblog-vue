f96e439cae180c5f59a63cf3033637d6042e15a0
Zmingg <zmingg@outlook.com>
2017-08-15 22:01:27 +0800

    nothing

--- src/router.js
+++ src/router.js
@@ -9,12 +9,12 @@ import Passport from './components/front/Passport.vue';
 
 
 export default new VueRouter({
-	// mode:'history',
-	routes:[
-		{ path: '/', component: Index },
-		{ path: '/blog/:cate?', component: Blog },
-		{ path: '/blog/search/:tag', component: Blog },
-		{ path: '/blog/:id/:title?', component: BlogShow },
-		{ path: '/passport',component:Passport},
-	]
+    // mode:'history',
+    routes:[
+        { path: '/', component: Index },
+        { path: '/blog/:cate?', component: Blog },
+        { path: '/blog/search/:tag', component: Blog },
+        { path: '/blog/:id/:title?', component: BlogShow },
+        { path: '/passport',component:Passport},
+    ]
 });
